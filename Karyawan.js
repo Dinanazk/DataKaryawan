b01844e5dc2e237b5d66f483aa551fc3e30180fb
dina <dina_20510002@stimata.ac.id>
2022-11-14 13:59:23 +0700

    Data Karyawan

---
+++ Karyawan.js
@@ -0,0 +1,23 @@
+const karyawan = [
+    {id:'1', nama:'Dina Nuraska', jabatan:'Ses. Prodi TI',
+    email:'dinaaska08@gmail.com', telp:'083845627879',
+    gaji:'1.000.000', foto:'https://placeimg.com/100/100/people'},
+
+    {id:'2', nama:'Ratna Wahyuningtyas', jabatan:'Ka. PUSKORDAT',
+    email:'ratna.wahyuningtyas@stimata.ac.id', telp:'085113458912',
+    gaji:'1.000.000', foto:'https://placeimg.com/100/100/nature'},
+
+    {id:'3', nama:'Anton Simanjutak', jabatan:'Ketua SENAT',
+    email:'anton@stimata.ac.id', telp:'088912345678',
+    gaji:'1.000.000', foto:'https://placeimg.com/100/100/tech'},
+
+    {id:'4', nama:'Yusuf Pangestu', jabatan:'Ka. Laboratorium',
+    email:'pangestu@stimata.ac.id', telp:'089667916862',
+    gaji:'1.000.000', foto:'https://placeimg.com/100/100/arch'},
+
+    {id:'5', nama:'Ahmad Prabowo', jabatan:'Dosen Homebase',
+    email:'ahmad.prabowo@stimata.ac.id', telp:'089765345755',
+    gaji:'1.000.000', foto:'https://placeimg.com/100/100/animals'},
+];
+
+export default karyawan;
